ae9832a9e8e587666116565676ace7e983f43497
halvorboe <halvor98@outlook.com>
2017-11-20 18:20:17 +0100

    Added tests

---
+++ src/ChessEngine/com/company/tests/PossibleTest.java
@@ -0,0 +1,18 @@
+package ChessEngine.com.company.tests;
+
+import ChessEngine.com.company.board.Possible;
+import ChessEngine.com.company.board.Value;
+import GUI.SaveAndLoad;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PossibleTest {
+
+    @Test
+    public void moveTest() {
+        byte[][] board = SaveAndLoad.loadBoard("data/board.txt");
+        Assert.assertTrue(
+                Possible.possible(board, new boolean[] {true, true, true, true, true}).size() == 20
+        );
+    }
+}
